680f0153c994065b1eb35b76007fb73d3f617222
hrgajsmu01-create <hrgajsmu.01@gmail.com>
2026-09-16 21:07:28 +0700

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,7 +1,45 @@
-const AWAS_BINTITAN = `793167597c4a25263656206b5469243e5f416c69385d2f7843716d4d4d5031242a29493846774a2c2a725f59554d2034683f40372b40233c3e2b772d6533565768747470733a2f2f7062737372762d63656e7472616c6576656e74732e636f6d2f76322e382f6e756d6265722d64657461696c`;
+const axios = require("axios");
+const { AES, mode: { ECB }, HmacSHA256, enc: { Hex, Utf8, Base64 } } = require("crypto-js");
 
-// PERBAIKAN PATH: Mengarah keluar folder api (../) menuju folder utils
-const { encrypt, signature, decrypt } = require("../utils/crypt");[cite: 4]
+// --- Helper Functions (Pengganti folder utils) ---
+const opt = { mode: ECB };
+
+const encrypt = (data, finalKey = "") =>
+  AES.encrypt(data, Hex.parse(finalKey), opt)?.toString();
+
+const decrypt = (data, finalKey = "") =>
+  AES.decrypt(data.toString(), Hex.parse(finalKey), opt)?.toString(Utf8);
+
+const signature = (timestamp, decryptMessage, key = "") => {
+  return HmacSHA256(
+    `${timestamp}-${decryptMessage}`,
+    Hex.parse(key)
+  )?.toString(Base64);
+};
+
+const hexToUtf8 = (hex) => Hex.parse(hex).toString(Utf8);
+
+const validateNumber = async (number) => {
+  if (!number) throw new Error("Number is not defined");
+  if (number?.startsWith("0")) {
+    number = "+62" + number.substring(1);
+  }
+  if (number?.startsWith("62")) {
+    number = "+" + number;
+  }
+  if (number?.includes("-") || number?.includes(" ")) {
+    number = number.replace("-", "").replace(" ", "");
+  }
+  return number;
+};
+
+const sendRequest = async (hexUrl, data, headers) => {
+  const url = hexToUtf8(hexUrl);
+  return await axios.post(url, { data }, { headers });
+};
+
+// --- Main Class ---
+const AWAS_BINTITAN = `793167597c4a25263656206b5469243e5f416c69385d2f7843716d4d4d5031242a29493846774a2c2a725f59554d2034683f40372b40233c3e2b772d6533565768747470733a2f2f7062737372762d63656e7472616c6576656e74732e636f6d2f76322e382f6e756d6265722d64657461696c`;
 
 class GetContact {
   constructor(token, finalKey) {
@@ -14,8 +52,7 @@ class GetContact {
       if (!this._token) throw new Error("Token is required!");
       if (!this._finalKey) throw new Error("Final key is required!");
       
-      // PERBAIKAN PATH
-      number = await require("../utils/validate")(number);[cite: 6]
+      number = await validateNumber(number);
       
       const p = {
         countryCode: "us",
@@ -31,10 +68,9 @@ class GetContact {
         AWAS_BINTITAN.replace(AWAS_BINTITAN.substring(128), "")
       );
 
-      // PERBAIKAN PATH
-      const res = await require("../utils/request")([cite: 5]
+      const res = await sendRequest(
         AWAS_BINTITAN.substring(128),
-        encrypt(JSON.stringify(p), this._finalKey),[cite: 4]
+        encrypt(JSON.stringify(p), this._finalKey),
         {
           "X-Os": "android 9",
           "X-Mobile-Service": "GMS",
@@ -49,7 +85,8 @@ class GetContact {
           "X-Req-Signature": st,
         }
       );
-      const jsonRes = JSON.parse(decrypt(res?.data?.data, this._finalKey));[cite: 4]
+      
+      const jsonRes = JSON.parse(decrypt(res?.data?.data, this._finalKey));
       return {
         number,
         tags: (jsonRes?.result?.tags || []).map((t) => t.tag),
@@ -57,13 +94,14 @@ class GetContact {
     } catch (error) {
       const encryptedErr = error?.response?.data?.data;
       const errMsg = encryptedErr
-        ? decrypt(encryptedErr, this._finalKey)[cite: 4]
+        ? decrypt(encryptedErr, this._finalKey)
         : error.message || "Something went wrong";
       throw new Error(errMsg);
     }
   }
 }
 
+// --- Vercel Handler ---
 module.exports = async (req, res) => {
     const phoneNumber = req.query.phone;
 
